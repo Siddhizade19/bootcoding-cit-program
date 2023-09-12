8df741c0501fd29a66c09f2d864e9db9e6553f7b
Siddhizade19 <144772955+Siddhizade19@users.noreply.github.com>
2023-09-12 16:04:18 +0530

    Add files via upload
    java code

---
+++ AdditionProgram.java
@@ -0,0 +1,15 @@
+public class AdditionProgram{
+public static void main(String args[]){
+System.out.println("My First Program in Java");
+int a=5, b=4;
+int add,sub,mul,div;
+add = a+b;
+sub = a-b;
+mul = a*b;
+div = a/b;
+System.out.println("Addition : "+ add);
+System.out.println("Substraction :"+ sub);
+System.out.println("Multiplication :"+ mul);
+System.out.println("Division :"+ div);
+}
+}
---
+++ IfElse2.java
@@ -0,0 +1,13 @@
+public class IfElse2{
+public static void main(String args[]){
+//find out the largest from given three numbers
+int a = 20,b = 30,c = 10;
+if(a>b && a>c){
+System.out.println(a +"is greater");
+}else if(b>a && b>c){
+System.out.println(b +"is greater");
+}else{
+System.out.println(c +"is greater");
+}		
+}
+}
